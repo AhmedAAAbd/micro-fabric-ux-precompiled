dce913e5c13fa3a1a50bd1c3a179f1af4d33b130
surinder singh <surinder83singh@gmail.com>
2018-06-14 23:31:25 +0530

    cleaning @BUILD dir before instalation

--- install.js
+++ install.js
@@ -1,7 +1,13 @@
 "use strict";
 var path  = require('path');
 var AdmZip = require('adm-zip');
+var fse = require('fs-extra')
 var zip = new AdmZip("./@BUILD.zip");
 var dest = path.join(__dirname, "../../@BUILD");
-zip.extractAllTo(dest, /*overwrite*/false);
 
+fse.emptyDir(dest, err => {
+	if (err)
+		return console.error(err)
+
+	zip.extractAllTo(dest, /*overwrite*/false);
+})
